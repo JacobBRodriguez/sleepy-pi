36836723327731b0a0de65c59fe281ffb522aa71
JacobBRodriguez <j.rodriguez7578@gmail.com>
2020-05-18 18:45:14 -0700

    Added temperature code for reading from GPIO 4

--- GPIO-drivers/temperature/temp.py
+++ GPIO-drivers/temperature/temp.py
@@ -1,1 +1,33 @@
+import os
+import glob
+import time
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+
+    temp = lines[1].split("=")
+    temp_string = temp[1]
+    temp_c = float(temp_string) / 1000.0
+    temp_f = temp_c * 9.0 / 5.0 + 32.0
+    return temp_c, temp_f
+
+while True:
+    print( read_temp())
+    time.sleep(1)
 
